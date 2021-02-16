f62d4c4490fb1bd4ba11f3185701eb8b43e54766
Angel Newton <xanewton@gmail.com>
2021-02-16 10:47:14 -0500

    Add the main menu

--- SwiftUINBAWatchDraft WatchKit Extension/Models/Views/ContentView.swift
+++ SwiftUINBAWatchDraft WatchKit Extension/Models/Views/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        List {
+            ForEach(0..<2) { _ in
+                NavigationLink(destination: EmptyView()) {
+                    ZStack {
+                        Image("draft-menu-background").resizable()
+                        VStack(spacing: -15) {
+                            Text("ROUND")
+                                .custom(font: .bold, size: 16)
+                            Text("1")
+                                .custom(font: .ultralight, size: 70)
+                        }.offset(x: -10, y: 10)
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowBackground(background)
+            }
+        }
+        .listStyle(CarouselListStyle())
+        .navigationBarTitle(Text("NBA Draft"))
+    }
+    
+    var background: some View {
+        Rectangle().fill(Color("CardBackground"))
     }
 }
 
