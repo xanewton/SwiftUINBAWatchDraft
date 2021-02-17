b475278551c6a69773549a8028f6d9b794f05c3d
Angel Newton <xanewton@gmail.com>
2021-02-16 21:51:43 -0500

    Add our card contents

--- SwiftUINBAWatchDraft WatchKit Extension/Models/Views/DraftList.swift
+++ SwiftUINBAWatchDraft WatchKit Extension/Models/Views/DraftList.swift
@@ -20,13 +20,63 @@ struct DraftList: View {
                     .custom(font: .bold, size: 20)
                 Divider()
             }
-            // Add next step here
             
+            ZStack {
+                ForEach((0...testPicks).reversed(), id: \.self) { index in
+                    fullCard
+                        .cardTransformed(self.currentIndex, card: index)
+                        .onTapGesture {
+                            self.isShowingDetail.toggle()
+                        }
+                        .sheet(isPresented: self.$isShowingDetail) {
+                            DraftPlayerDetailView()
+                        }
+                }
+            }
+            .focusable(true)
+            .digitalCrownRotation(
+                $currentIndex.animation(),
+                from: 0.0,
+                through: Double(testPicks - 1),
+                by: 1.0,
+                sensitivity: .low
+            )
         }
         .navigationBarTitle(Text("By Round"))
     }
     
-    // add topCard here
+    var topCard: some View {
+        HStack {
+            Image("pelicans").frame(height: 56)
+            Spacer()
+            Text(String(format: "%02d", 1))
+                .custom(font: .bold, size: 50)
+        }
+        .frame(height: 48)
+        .background(Color("pelicans"))
+    }
+    
+    var bottomCard: some View {
+        VStack(spacing: -5) {
+            Text("ZION")
+                .custom(font: .ultralight, size: 13)
+            Text("WILLIAMS")
+                .custom(font: .bold, size: 20)
+            Text("POWER FORWARD")
+                .custom(font: .ultralight, size: 10)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .frame(height: 60, alignment: .center)
+        .background(Color.white)
+        .foregroundColor(.black)
+    }
+
+    var fullCard: some View {
+        VStack(alignment: .leading, spacing: -3) {
+            topCard
+            bottomCard
+        }
+    }
 }
 
 struct DraftList_Previews: PreviewProvider {
